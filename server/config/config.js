503a00f27052d86ed200ab25e85bad66829a1291
Jhon Jairo Zabala Bohorquez <jhonnyzb1@hotmail.com>
2019-12-14 16:23:34 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 }
 else {
-    urlDB = 'mongodb+srv://jhonny:Lp5VylfNWggkGZxN@cluster0-lfg1o.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
