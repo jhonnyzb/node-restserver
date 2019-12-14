be209e871a629c864167eed19b4d77e2ddf27e90
Jhon Jairo Zabala Bohorquez <jhonnyzb1@hotmail.com>
2019-12-14 15:39:40 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,20 @@
 //puerto
 
 process.env.PORT = process.env.PORT || 3000
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+//Base de datos
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+}
+else {
+    urlDB = 'mongodb+srv://jhonny:Lp5VylfNWggkGZxN@cluster0-lfg1o.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB
