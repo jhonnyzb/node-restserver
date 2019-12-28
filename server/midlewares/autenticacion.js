bd125c80a6abf138476e2942ed4297985f9cfbfa
Jhon Jairo Zabala Bohorquez <jhonnyzb1@hotmail.com>
2019-12-28 15:29:34 -0500

    subida de archivos

--- server/midlewares/autenticacion.js
+++ server/midlewares/autenticacion.js
@@ -43,7 +43,30 @@ let verificaRolAdmin = (req, res, next) => {
 
 }
 
+//Verificar token img
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario
+        next()
+    })
+}
+
+
+
 module.exports = {
     verificaToken,
-    verificaRolAdmin
+    verificaRolAdmin,
+    verificaTokenImg
 }
